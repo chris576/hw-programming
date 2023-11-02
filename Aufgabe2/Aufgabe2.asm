f88f2892ce380dd9a8ee05b04de6f3e6ec282438
chris576 <christopher.huebna@gmx.de>
2023-11-02 17:18:48 +0100

    Aufgabe 2 weiter

--- Aufgabe2/Aufgabe2.asm
+++ Aufgabe2/Aufgabe2.asm
@@ -25,10 +25,12 @@ START:
     sar bl, 1
     sar bl, 1
     sar bl, 1
+    and bl, 00001111b
     add cl, bl
-    add bl, cl
     mov al, cl
     out LED, al
-    mov al, bl
+    mov al, [TAB7]
     out firstDis, al
     jmp START
+
+TAB7 db 00111111b, 01100000b, 11011010b, 11110010b, 01100110b, 10110110b, 10111110b, 11100000b, 11111110b, 11110110b, 11110110b, 00111110b, 10011100b, 01111010b, 10011110b, 10001110b
